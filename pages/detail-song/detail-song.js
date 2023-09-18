7d68c76e4633e1ff124d1b7f6f63463fe3173deb
hanwu <hanwu1314@outlook.com>
2023-09-18 19:36:47 +0800

    榜单详情-列表数据获取

--- pages/detail-song/detail-song.js
+++ pages/detail-song/detail-song.js
@@ -1,10 +1,35 @@
 // pages/detail-song/detail-song.js
 import recommendStore from "../../store/recommendStore"
+import rankingStore from "../../store/rankingStore"
 Page({
- data:{
+  data: {
+    type: "ranking",
+    key: "newRanking",
+    id: "",
 
- },
- onLoad(){
-   
- }
+    songInfo: {}
+  },
+  onLoad(options) {
+    const type = options.type
+    this.data.type = type
+    console.log("type: ", type);
+
+    if (type == "ranking") {
+      const key = options.key
+      this.data.key = key
+      rankingStore.onState(key, this.handleRanking)
+    }
+  },
+  handleRanking(value) {
+    this.setData({ songInfo: value })
+    wx.setNavigationBarTitle({
+      title: value.name,
+    })
+  },
+  onUnload() {
+    if (this.data.type === "ranking") {
+      rankingStore.offState(this.data.key, this.handleRanking)
+
+    }
+  }
 })
