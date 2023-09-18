073c3a7dee58c7536d33d95aecc6a65846459e18
hanwu <hanwu1314@outlook.com>
2023-09-18 18:16:06 +0800

    歌单详情

--- pages/detail-menu/detail-menu.js
+++ pages/detail-menu/detail-menu.js
@@ -1,66 +1,27 @@
 // pages/detail-menu/detail-menu.js
+import { getSongMenuTag, getSongMenuList } from "../../services/music"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    songMenus: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad() {
+    this.fetchGetAllMenuList()
   },
+  //  发送网络请求
+  async fetchGetAllMenuList() {
+    const tagRes = await getSongMenuTag()
+    const tags = tagRes.tags
+    // 根据tag获取歌单
+    const allPromises = []
+    for (const tag of tags) {
+      const promise = getSongMenuList(tag.name)
+      allPromises.push(promise)
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    // 3.获取到所有的数据之后, 调用一次setData
+    Promise.all(allPromises).then(res => {
+      console.log(res);
+      this.setData({ songMenus: res })
+    })
   }
 })
