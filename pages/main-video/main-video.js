bde6d3b707cb1ddab1735857378911d8e4395dba
hanwu <hanwu1314@outlook.com>
2023-09-18 12:20:59 +0800

    首页数据下拉刷新上拉加载功能

--- pages/main-video/main-video.js
+++ pages/main-video/main-video.js
@@ -2,13 +2,36 @@
 import { getTopMV } from "../../services/video"
 Page({
   data: {
-    videoList: []
+    videoList: [],
+    offset: 0,
+    /**是否有更多 */
+    hasMore: true
   },
   onLoad() {
     this.fetchTopMV()
   },
   async fetchTopMV() {
-    const res = await getTopMV()
-    this.setData({ videoList: res.data })
-  }
+    const res = await getTopMV(this.data.offset)
+    // 新数据追加
+    const newVideoList = [...this.data.videoList, ...res.data]
+    this.setData({ videoList: newVideoList })
+    this.data.offset = this.data.videoList.length
+    this.data.hasMore = res.hasMore
+  },
+  onReachBottom() {
+    if (!this.data.hasMore) return
+    this.fetchTopMV()
+  },
+  async onPullDownRefresh() {
+    // 1.清空之前的数据
+    this.setData({ videoList: [] })
+    this.data.offset = 0
+    this.data.hasMore = true
+
+    // 2.重新请求新的数据
+    await this.fetchTopMV()
+
+    // 3.停止下拉刷新
+    wx.stopPullDownRefresh()
+  },
 })
--- services/video.js
+++ services/video.js
@@ -1,10 +1,10 @@
 import { hwRequest } from './index'
-export function getTopMV(offset = 10, limit = 20) {
+export function getTopMV(offset = 0, limit = 20) {
   return hwRequest.get({
     url: "/top/mv",
     data: {
-      limit: offset,
-      offset: limit
+      limit: limit,
+      offset: offset
     }
   })
 
