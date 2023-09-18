85a987c10b62c281d1de369f2ef7d6e1acd9b211
hanwu <hanwu1314@outlook.com>
2023-09-18 17:03:37 +0800

    热门歌曲数据共享

--- pages/detail-song/detail-song.js
+++ pages/detail-song/detail-song.js
@@ -1,66 +1,16 @@
 // pages/detail-song/detail-song.js
+import recommendStore from "../../store/recommendStore"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    songs: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad() {
+    recommendStore.onState("recommendSongs", this.handleRecommendSongs)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload() {
-
+    recommendStore.offState("recommendSongs", this.handleRecommendSongs)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  handleRecommendSongs(value) {
+    this.setData({ songs: value })
   }
 })
