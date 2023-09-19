e8455aa6038bb80593d2823a9dd59ea6acc074d2
hanwu <hanwu1314@outlook.com>
2023-09-19 17:28:42 +0800

    歌词匹配

--- pages/music-player/music-player.js
+++ pages/music-player/music-player.js
@@ -1,5 +1,6 @@
 // pages/music-player/music-player.js
 import { getSongDetail, getSongLyric } from "../../services/player"
+import { parseLyric } from "../../utils/parse-lyric"
 import { throttle } from 'underscore'
 
 const app = getApp()
@@ -11,7 +12,7 @@ Page({
     /**当前歌曲 */
     currentSong: {},
     /**歌词 */
-    lrcString: "",
+    lyricInfos: "",
     /**当前页 */
     currentPage: 0,
     contentHeight: 0,
@@ -21,7 +22,10 @@ Page({
     durationTime: 0,
     sliderValue: 0,
     isSliderChanging: false,
-    isWaiting: false
+    isWaiting: false,
+    isPlaying: true,
+    currentLyricText: "",
+    currentLyricIndex: -1
   },
   onLoad(options) {
     this.setData({
@@ -39,17 +43,34 @@ Page({
     })
     // 获取歌词信息
     getSongLyric(id).then(res => {
-      this.setData({ lrcString: res.lrc })
+      const lrcString = res.lrc.lyric
+      const lyricInfos = parseLyric(lrcString)
+      this.setData({ lyricInfos })
     })
     // 播放当前歌曲
     audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
     audioContext.autoplay = true
     // 监听播放进度
-    const throttleUpdateProgress = throttle(this.updateProgress, 500, { leading: false })
+    const throttleUpdateProgress = throttle(this.updateProgress, 500, { leading: false, trailing: false })
     audioContext.onTimeUpdate(() => {
+      // 更新歌曲的进度
       if (!this.data.isSliderChanging && !this.data.isWaiting) {
         throttleUpdateProgress()
       }
+      if (!this.data.lyricInfos.length) return
+      // 匹配正确的歌词 - 找到比当前时间大的减去1就是当前歌词
+      let index = -1
+      for (let i = 0; i < this.data.lyricInfos.length; i++) {
+        const info = this.data.lyricInfos[i]
+        if (info.time > audioContext.currentTime * 1000) {
+          index = i - 1
+          break
+        }
+      }
+      console.log(index, this.data.lyricInfos[index].text);
+      if (index === this.data.currentLyricIndex) return
+      const currentLyricText = this.data.lyricInfos[index].text
+      this.setData({ currentLyricText, currentLyricIndex: index })
     })
     audioContext.onWaiting(() => {
       audioContext.pause()
@@ -60,11 +81,11 @@ Page({
   },
   /**更新进度条 */
   updateProgress() {
-    // 记录当前时间
-    this.setData({ currentTime: audioContext.currentTime * 1000 })
-    // 修改滑块值
     const sliderValue = this.data.currentTime / this.data.durationTime * 100
-    this.setData({ sliderValue })
+    this.setData({
+      currentTime: audioContext.currentTime * 1000,
+      sliderValue
+    })
   },
   // 事件监听
   onSwiperChange(event) {
@@ -84,5 +105,14 @@ Page({
     const currentTime = value / 100 * this.data.durationTime
     audioContext.seek(currentTime / 1000)
     this.setData({ currentTime, isSliderChanging: false, sliderValue: value })
+  },
+  onPlayOrPauseTap() {
+    if (!audioContext.paused) {
+      audioContext.pause()
+      this.setData({ isPlaying: false })
+    } else {
+      audioContext.play()
+      this.setData({ isPlaying: true })
+    }
   }
 })
